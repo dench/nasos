dbc8d47e2556cdd1268173901ba79e03369e296a
Alexey Dench <dench@i.ua>
2017-07-08 17:33:37 +0300

    Cache

--- admin/controllers/CategoryController.php
+++ admin/controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace app\admin\controllers;
 
 use dench\image\models\Image;
+use dench\language\models\Language;
 use dench\sortable\actions\SortingAction;
 use Yii;
 use app\models\Category;
@@ -108,6 +109,9 @@ class CategoryController extends Controller
                     $image->save(false);
                 }
                 Yii::$app->session->setFlash('success', Yii::t('app', 'Information added successfully'));
+                foreach (Language::find()->select('id')->column() as $lang) {
+                    Yii::$app->cache->delete('_categories-' . $lang);
+                }
                 return $this->redirect(['index']);
             }
         }
@@ -155,6 +159,9 @@ class CategoryController extends Controller
                     $image->save(false);
                 }
                 Yii::$app->session->setFlash('success', Yii::t('app', 'Information has been saved successfully'));
+                foreach (Language::find()->select('id')->column() as $lang) {
+                    Yii::$app->cache->delete('_categories-' . $lang);
+                }
                 return $this->redirect(['index']);
             }
         }
