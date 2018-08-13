192c314f3a366ac6ee29f3bdcbb33777cc3d2aef
Alexey Dench <dench@i.ua>
2018-08-13 10:04:25 +0300

    Send configurator to email

--- models/QuestionnaireForm.php
+++ models/QuestionnaireForm.php
@@ -63,7 +63,7 @@ class QuestionnaireForm extends Questionnaire
                 '<b>' . Yii::t('questionnaire', 'Performance') . '</b>: ' . implode(', ', $performance),
                 '<b>' . Yii::t('questionnaire', 'Supply') . '</b>: ' . implode(', ', $supply),
                 '<b>' . Yii::t('questionnaire', 'Level') . '</b>: ' . self::levelList()[$this->level],
-            ], ['encode' => false]) . print_r($supply, 1) . print_r($performance, 1) . $this->supply . $this->performance;
+            ], ['encode' => false]) . print_r($supply, 1) . print_r($performance, 1) . $this->supply . ',' . $this->performance;
 
             Yii::$app->mailer->compose()
                 ->setTo(Yii::$app->params['adminEmail2'])
