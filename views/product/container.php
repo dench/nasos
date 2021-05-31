011502263fe3f009c3d439cf172434f44fd7ced9
Alexey Dench <dench@i.ua>
2021-05-31 10:53:16 +0300

    Button order card

--- views/product/container.php
+++ views/product/container.php
@@ -5,12 +5,27 @@
 /* @var $similar dench\products\models\Product[] */
 /* @var $viewed boolean */
 
+use yii\bootstrap\Html;
+use yii\bootstrap\Modal;
 use yii\helpers\Url;
 
 echo $this->render('_breadcrumbs', [
     'model' => $model,
 ]);
 
+$js = <<< JS
+$('.modal-callback-open').mousedown(function(){
+    $('#modal-callback').modal('show').find('#modal-callback-content').load($(this).attr('data-target'));
+});
+JS;
+Modal::begin([
+    'id' => 'modal-callback',
+    'header' => '<h3>' . Yii::t('app', 'Callback') . '</h3>',
+]);
+echo Html::tag('div', '', ['id' => 'modal-callback-content']);
+Modal::end();
+
+$this->registerJs($js);
 ?>
 <div class="container page product">
     <h1 class="page-title"><?= $model->h1 ?></h1>
@@ -21,7 +36,7 @@ echo $this->render('_breadcrumbs', [
             ]) ?>
         </div>
         <div class="col-md-7">
-            <button class="btn btn-primary btn-block btn-lg modal-callback-open" data-target="<?= Url::to(['/site/callback']) ?>"><?= Yii::t('app', 'To order') ?></button>
+            <button class="btn btn-primary btn-lg modal-callback-open" data-target="<?= Url::to(['/site/callback']) ?>"><?= Yii::t('app', 'To order') ?></button>
             <br>
             <?= $this->render('_feature_simple', [
                 'model' => $model,
